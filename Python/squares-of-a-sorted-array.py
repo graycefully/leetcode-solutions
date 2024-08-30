982fd1fd545480e345b1ee75df94c728c8a595ca
graycefully <grayce.zhu@outlook.com>
2024-08-30 14:15:17 -0400

    Create squares-of-a-sorted-array.py

---
+++ Python/squares-of-a-sorted-array.py
@@ -0,0 +1,19 @@
+# Time: O(N)
+# Space: O(N)
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        l, r = 0, n - 1
+        result = [0] * n
+
+        for i in range(n - 1, -1, -1):
+            if abs(nums[l]) > abs(nums[r]):
+                result[i] = nums[l] ** 2
+                l += 1
+            else:
+                result[i] = nums[r] ** 2
+                r -= 1
+
+        return result
+        
