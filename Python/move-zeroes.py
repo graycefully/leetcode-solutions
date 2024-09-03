ff31825ef7e2b99e2d5a436f146b97198668b9b5
graycefully <grayce.zhu@outlook.com>
2024-09-03 16:51:30 -0400

    Create move-zeroes.py

---
+++ Python/move-zeroes.py
@@ -0,0 +1,14 @@
+# Time: O(N)
+# Space: O(1)
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[r], nums[l] = nums[l], nums[r]
+                l += 1
+        
